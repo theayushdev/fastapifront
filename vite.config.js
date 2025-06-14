67abdaa480a58d09eea439164939de9a91341b5c
Ayush Saraswat <44120068+theayushdev@users.noreply.github.com>
2025-06-14 15:39:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/fastapifront/',
   plugins: [react()],
-  base: '/fastapifront/'
+  
 })
 
 
